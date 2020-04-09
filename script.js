033d592554f1b13c4ce6165d84b3791757555ac6
demographerist <55778212+demographerist@users.noreply.github.com>
2020-04-09 16:14:53 -0500

    Adding starting project

---
+++ script.js
@@ -0,0 +1,30 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var randomButton = document.getElementById("random");
+
+function changeBackground() {
+	body.style.background =
+		"linear-gradient(to right, " 
+		+ color1.value 
+		+ ", " 
+		+ color2.value 
+		+ ")";
+
+		css.textContent = body.style.background + ";";
+}
+function randomColors() {
+	color1.value = "#" + Math.floor(Math.random()*16777215).toString(16);
+	color2.value = "#" + Math.floor(Math.random()*16777215).toString(16);
+	changeBackground();
+}
+
+
+color1.addEventListener("input", changeBackground);
+
+color2.addEventListener("input", changeBackground);
+
+randomButton.addEventListener("click", randomColors);
+
+changeBackground();
